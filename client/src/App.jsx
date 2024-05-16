2be58d0af8204ada76a9ad286c1154fe100a9cd7
Andy <yoongi9289@gmail.com>
2024-05-15 21:56:55 -0600

    moved all routes to App.jsx file for vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,31 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import AllRoutes from "./allRoutes";
+// import AllRoutes from "./allRoutes";
+import Index from "./pages/index";
+import Register from "./pages/register";
+import Login from "./pages/login";
+import Secret from "./pages/secret";
+// import AccountLink from "./pages/accountLink";
+import VerificationCode from "./pages/verify";
+import Dashboard from "./pages/Dashboard";
+import ResetPassPage from "./pages/newPassword";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <AllRoutes />
+        {/* <AllRoutes /> */}
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/verify" element={<VerificationCode />} />
+          <Route path="/secret" element={<Secret />} />
+          <Route path="/dashboard/:cat" element={<Dashboard />} />
+          <Route path="/password-reset/:token" element={<ResetPassPage />} />
+        </Routes>
       </AuthProvider>
     </BrowserRouter>
   );
