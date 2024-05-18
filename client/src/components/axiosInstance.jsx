510be71df8b11da968d7ceb5a5832152519d9117
Andy <yoongi9289@gmail.com>
2024-05-18 12:21:04 -0600

    add timeout for axios instance

--- client/src/components/axiosInstance.jsx
+++ client/src/components/axiosInstance.jsx
@@ -6,6 +6,7 @@ const customAxios = axios.create({
   // Base url for vercel
   baseURL: "https://user-auth-server-three.vercel.app",
   withCredentials: true,
+  timeout: 3000, // in ms, after 3sec will throw error if call is not returned
 });
 
 export default customAxios;
